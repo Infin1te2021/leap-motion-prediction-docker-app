ddc8e67ec9b213af508cc7b4704266eac59d7c31
James Zhang <zh398981121@outlook.com>
2023-05-29 10:58:23 +0800

    Initial commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+const host = process.env.HOST;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express Server');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://${host}:${port}`);
+});
